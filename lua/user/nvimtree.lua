cbb61dab047841479d0537c9fc9809093e955aa3
Curtis Matz <curtis@matzzone.com>
2023-03-19 10:02:55 -0400

    allow nvimtree to open on folder like netrw.

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -8,7 +8,7 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 require "nvim-tree".setup({
-  disable_netrw = true,
+  disable_netrw = false,
   actions = {
     open_file = {
       quit_on_open = true,

785ac6e1f1663b65813fb9015b4f1086984b662d
Curtis Matz <curtis@matzzone.com>
2023-03-20 19:35:10 -0400

    moved on_attach within setup.

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -7,25 +7,6 @@ end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-local on_attach = function(bufnr)
-  local api = require('nvim-tree.api')
-
-  local function opts(desc)
-    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-  end
-
-  -- default keymap
-  api.config.mappings.default_on_attach(bufnr)
-
-  -- my custom mappings
-  vim.keymap.set('n', 'A', api.tree.expand_all, opts('Expand All'))
-  vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
-  vim.keymap.set('n', 'D', '', { buffer = bufnr })
-  vim.keymap.del('n', 'D', { buffer = bufnr })
-  vim.keymap.set('n', 'E', '', { buffer = bufnr })
-  vim.keymap.del('n', 'E', { buffer = bufnr })
-end
-
 require "nvim-tree".setup({
   disable_netrw = false,
   actions = {
@@ -33,5 +14,22 @@ require "nvim-tree".setup({
       quit_on_open = true,
     },
   },
-  on_attach = on_attach,
+  on_attach = function(bufnr)
+    local api = require('nvim-tree.api')
+
+    local function opts(desc)
+      return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- default keymap
+    api.config.mappings.default_on_attach(bufnr)
+
+    -- my custom mappings
+    vim.keymap.set('n', 'A', api.tree.expand_all, opts('Expand All'))
+    vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+    vim.keymap.set('n', 'D', '', { buffer = bufnr })
+    vim.keymap.del('n', 'D', { buffer = bufnr })
+    vim.keymap.set('n', 'E', '', { buffer = bufnr })
+    vim.keymap.del('n', 'E', { buffer = bufnr })
+  end,
 })

8a5baae94030d5259aa0ef8ef16a90e630ae5abb
Curtis Matz <curtis@matzzone.com>
2023-03-19 20:07:38 -0400

    nvim-tree on_attach function ready for future work.

--- lua/user/nvimtree.lua
+++ lua/user/nvimtree.lua
@@ -7,6 +7,21 @@ end
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+local on_attach = function(bufnr)
+
+  local api = require('nvim-tree.api')
+
+  local function opts(desc)
+    return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+  end
+  -- default keymap
+  api.config.mappings.default_on_attach(bufnr)
+  -- my custom mappings
+  vim.keymap.set('n', 'A', api.tree.expand_all, opts('Expand All'))
+  vim.keymap.set('n', 'D', '', { buffer = bufnr })
+  vim.keymap.del('n', 'D', { buffer = bufnr })
+end
+
 require "nvim-tree".setup({
   disable_netrw = false,
   actions = {
@@ -14,4 +29,5 @@ require "nvim-tree".setup({
       quit_on_open = true,
     },
   },
+  on_attach = on_attach,
 })
